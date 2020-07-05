c41e71fb413f524bada7d282bae6bf2b8ee5c742
nhatvu148 <nhatvu148@gmail.com>
2020-07-05 19:44:50 +0900

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var fileSource = new FileInfo(@"C:\Users\nhatv\Work\VisualBasic\New-Data.xlsx");
+            var fileDestination = new FileInfo(@"C:\Users\nhatv\Work\VisualBasic\Reports.xlsm");
+
+            using (var excelFileSource = new ExcelPackage(fileSource))
+            using (var excelFileDestination = new ExcelPackage(fileDestination))
+            {
+                var worksheetSource = excelFileSource.Workbook.Worksheets[0];
+
+                var cellRange = worksheetSource.Cells["A2:D9"];
+
+                var worksheetDestination = excelFileDestination.Workbook.Worksheets[0];
+
+                var destination = worksheetDestination.Cells["A2"];
+
+                cellRange.Copy(destination);
+
+                excelFileDestination.Save();
+            }
+        }
+    }
+}
