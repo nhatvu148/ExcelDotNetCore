e83d6a63d9041af82397b90d454eddfa795cd4ed
NHAT VU <nhatvu148@NHATs-MacBook-Pro.local>
2020-07-13 00:06:50 +0900

    finish copy whole worksheet

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -20,7 +20,10 @@ namespace ConsoleApp2
             using (var excelFileDestination = new ExcelPackage(fileDestination))
             {
                 var worksheetSource = excelFileSource.Workbook.Worksheets[0];
-                var cellRange = worksheetSource.Cells["D6:D35"];
+                var nameSource = worksheetSource.Cells["C3"];
+                var cellRange1 = worksheetSource.Cells["D6:D35"];
+                var cellRange2 = worksheetSource.Cells["E6:E35"];
+                var cellRange3 = worksheetSource.Cells["F6:F35"];
 
                 foreach (var worksheet in excelFileDestination.Workbook.Worksheets)
                 {
@@ -29,16 +32,68 @@ namespace ConsoleApp2
 
                 var worksheetDestination = excelFileDestination.Workbook.Worksheets[0];
 
-                var destination = worksheetDestination.Cells["D8:D37"];
+                var nameDestination = worksheetDestination.Cells["C5:I5"];
+                var destination1 = worksheetDestination.Cells["D8:D37"];
+                var destination2 = worksheetDestination.Cells["G8:G37"];
+                var destination3 = worksheetDestination.Cells["J8:J37"];
 
-                foreach (var cell in destination)
-                {
-                    string range = cell.ToString() + ":" + cell.ToString().Substring(0, 1) + cell.ToString().Substring(1);
-                    Console.WriteLine(range);
+                if (worksheetDestination.Cells["C5:I5"].Merge) {
+                    worksheetDestination.Cells["C5:I5"].Merge = false;
+                    nameSource.Copy(nameDestination);
+                    worksheetDestination.Cells["C5:I5"].Merge = true;
                 }
 
-                // cellRange.Copy(destination);
+                foreach (var cell in destination1)
+                {
+                    string index = cell.ToString().Substring(1);
+                    // Unmerge
+                    string range1 = $"D{index}:F{index}";
+                    if (worksheetDestination.Cells[range1].Merge)
+                    {
+                        worksheetDestination.Cells[range1].Merge = false;
+                    }
 
+                    string range2 = $"G{index}:I{index}";
+                    if (worksheetDestination.Cells[range2].Merge)
+                    {
+                        worksheetDestination.Cells[range2].Merge = false;
+                    }
+
+                    string range3 = $"J{index}:L{index}";
+                    if (worksheetDestination.Cells[range3].Merge)
+                    {
+                        worksheetDestination.Cells[range3].Merge = false;
+                    }
+                }
+
+                cellRange1.Copy(destination1);
+                cellRange2.Copy(destination2);
+                cellRange3.Copy(destination3);
+
+                foreach (var cell in destination1)
+                {
+                    string index = cell.ToString().Substring(1);
+
+                    string range1 = $"D{index}:F{index}";
+                    string range2 = $"G{index}:I{index}";
+                    string range3 = $"J{index}:L{index}";
+
+                    // Re-merge
+                    if (!worksheetDestination.Cells[range1].Merge)
+                    {
+                        worksheetDestination.Cells[range1].Merge = true;
+                    }
+
+                    if (!worksheetDestination.Cells[range2].Merge)
+                    {
+                        worksheetDestination.Cells[range2].Merge = true;
+                    }
+
+                    if (!worksheetDestination.Cells[range3].Merge)
+                    {
+                        worksheetDestination.Cells[range3].Merge = true;
+                    }
+                }
                 excelFileDestination.Save();
             }
         }
